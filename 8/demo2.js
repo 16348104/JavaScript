c02fdae788c0ed2aaa33a0c09b3567583c866982
16348104 <16348104@qq.com>
2017-01-23 16:09:49 +0800

    update

---
+++ 8/demo2.js
@@ -0,0 +1,16 @@
+/**
+ * Created by xyzhang on 2017/1/23.
+ */
+var box = [
+    {
+        name: "zhaoxue",
+        age: 34
+    },
+    [3, 4, 5, 6, "北京", new Object()],
+    "计算机",
+    25 + 25,
+    new Array(1, 2, 3)
+];
+alert(box);
+
+alert(box[0].name);
