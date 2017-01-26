cc2fb9b4aa9d86e7e29c35fe5b9bb2ca0b22adf5
16348104 <16348104@qq.com>
2017-01-26 22:12:51 +0800

    update12

--- 12/demo2.js
+++ 12/demo2.js
@@ -5,3 +5,23 @@
  }
  alert(window.box);
  alert(window.setbox());*/
+
+/*
+if (true) {
+    var b = 'Lee';                  //if没有局部作用域
+}
+alert(b);
+
+for (var i=0;i<10;i++) {
+    var p = 'zhao';                 //for没有局部作用域
+}
+alert(i);
+alert(p);*/
+
+var box = 'blue';
+function getbox() {
+    // var box = 'zhao';      //通过向上搜索作用域，如果加上var box = 'zhao'返回值为zhao
+    return box;              //box代表全局
+                              //
+}
+alert(getbox());
