c0ffb77708cf68091db8b4b9807ba023c37ee9fc
16348104 <16348104@qq.com>
2017-01-26 22:27:20 +0800

    update12

--- 12/demo2.js
+++ 12/demo2.js
@@ -7,21 +7,34 @@
  alert(window.setbox());*/
 
 /*
-if (true) {
-    var b = 'Lee';                  //if没有局部作用域
-}
-alert(b);
+ if (true) {
+ var b = 'Lee';                  //if没有局部作用域
+ }
+ alert(b);
 
-for (var i=0;i<10;i++) {
-    var p = 'zhao';                 //for没有局部作用域
-}
-alert(i);
-alert(p);*/
+ for (var i=0;i<10;i++) {
+ var p = 'zhao';                 //for没有局部作用域
+ }
+ alert(i);
+ alert(p);*/
 
-var box = 'blue';
-function getbox() {
-    // var box = 'zhao';      //通过向上搜索作用域，如果加上var box = 'zhao'返回值为zhao
-    return box;              //box代表全局
-                              //
+/*
+ var box = 'blue';
+ function getbox() {
+ // var box = 'zhao';      //通过向上搜索作用域，如果加上var box = 'zhao'返回值为zhao
+ return box;              //box代表全局
+ //
+ }
+ alert(getbox());
+ */
+var box = 'lee';
+function setBox() {
+
+    function setColor() {
+        var p = 'zhao';
+        alert(p);
+        return 123;
+    }
+    return setColor();
 }
-alert(getbox());
+alert(window.setBox());
