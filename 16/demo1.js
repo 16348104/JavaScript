9c93025982d0373c9a619c879619ad6fbc86fd26
16348104 <16348104@qq.com>
2017-01-31 01:30:04 +0800

    update16

--- 16/demo1.js
+++ 16/demo1.js
@@ -6,10 +6,18 @@
  alert(b());*/
 
 /*
- (function () {                 //通过自我执行匿名函数
+ (function () {                         //通过自我执行匿名函数
  alert('lee');
  })();
  */
+/*var  box= (function () {                 //自我执行匿名函数返回值给变量
+    return 'lee';
+})();
+alert(box);*/
+alert((function () {
+    return 'lee';
+})());
+/*
 function counter() {
     var n = 0;
     return {
@@ -27,3 +35,4 @@ alert(d.count());
 c.reset();
 alert(c.count());
 alert(d.count());
+*/
