24507f68b027d277d741ac403355699c1a9004a5
16348104 <16348104@qq.com>
2017-01-31 02:22:12 +0800

    update16

--- 16/demo1.js
+++ 16/demo1.js
@@ -11,28 +11,60 @@
  })();
  */
 /*var  box= (function () {                 //自我执行匿名函数返回值给变量
-    return 'lee';
-})();
-alert(box);*/
-alert((function () {
-    return 'lee';
-})());
-/*
-function counter() {
-    var n = 0;
-    return {
-        count: function () {
-            return n++;
-        },
-        reset: function () {
-            n = 0;
-        }
+ return 'lee';
+ })();
+ alert(box);*/
+/*alert((function () {
+ return 'lee';
+ })());*/
+/*(function (age) {                     //自我执行匿名函数的传参
+ alert(age);
+ })(100);*/
+/*function box() {
+ return function () {             //闭包
+ return 'lee';
+ }
+ }
+ alert(box()());*/
+function box() {                 //通过闭包返回局部变量
+    var age = 100;
+    return function () {
+        return age;
     };
 }
-var c = counter(), d = counter();
-alert(c.count());
-alert(d.count());
-c.reset();
-alert(c.count());
-alert(d.count());
-*/
+// alert(box()());
+var b = box();                  //另一种调用匿名函数的方式
+alert(b());
+
+
+var age = 100;                   //使用全局变量累加
+function b() {
+    age++;
+}
+alert(age);
+b();
+alert(age);
+b();
+alert(age);
+
+
+
+/*
+ function counter() {
+ var n = 0;
+ return {
+ count: function () {
+ return n++;
+ },
+ reset: function () {
+ n = 0;
+ }
+ };
+ }
+ var c = counter(), d = counter();
+ alert(c.count());
+ alert(d.count());
+ c.reset();
+ alert(c.count());
+ alert(d.count());
+ */
