f4a6fb11ec42ec4b873525eb6834d95eb6659ea3
16348104 <16348104@qq.com>
2017-01-31 01:21:42 +0800

    update16

--- 16/demo1.js
+++ 16/demo1.js
@@ -1,13 +1,29 @@
-
 /*
-//把匿名函数赋值给变量
-var b = function () {
-    return 'lee';
-};
-alert(b());*/
+ //把匿名函数赋值给变量
+ var b = function () {
+ return 'lee';
+ };
+ alert(b());*/
 
 /*
  (function () {                 //通过自我执行匿名函数
  alert('lee');
  })();
  */
+function counter() {
+    var n = 0;
+    return {
+        count: function () {
+            return n++;
+        },
+        reset: function () {
+            n = 0;
+        }
+    };
+}
+var c = counter(), d = counter();
+alert(c.count());
+alert(d.count());
+c.reset();
+alert(c.count());
+alert(d.count());
