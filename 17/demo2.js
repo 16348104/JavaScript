ee1340711aff8217cb5f40db8ff961b5a72de572
16348104 <16348104@qq.com>
2017-02-03 11:23:54 +0800

    update17

---
+++ 17/demo2.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dell on 2017/2/3.
+ */
+/*
+ alert(window.innerWidth);
+ //alert(window.innerHeight);*/
+//alert(document.compatMode);
+//alert(typeof document.compatMode);
+
+//取得各自浏览器可是窗口的大小
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+if (typeof width !== 'number') {
+    if (document.compatMode == 'CSS1Compat') {                             //返回CSS1Compat为标准摸式
+        width = document.documentElement.clientWidth;
+        height = document.documentElement.clientHeight;
+    } else {                                                            //非标准模式采用body
+        width = document.body.clientWidth;
+        height = document.body.clientHeight;
+    }
+}
+alert(width);
+alert(height);
--- 17/opener.js
+++ 17/opener.js
@@ -1,6 +1,8 @@
 /**
  * Created by xyzhang on 2017/2/2.
  */
+ /*
 document.onclick=function () {
     opener.document.writeln('子窗口操作父窗口!');
-}
+}*/
+alert(document.compatMode);
