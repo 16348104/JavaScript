0b73482e2023f5439f82ddcbff0967875a42f7a9
16348104 <16348104@qq.com>
2017-02-05 12:14:09 +0800

    update20

---
+++ 20/demo1.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    // alert(Node.ELEMENT_NODE);
+    // alert(Node.TEXT_NODE);
+    // alert(document);
+    alert(document.documentElement);
+    alert(document.nodeType);
+};
