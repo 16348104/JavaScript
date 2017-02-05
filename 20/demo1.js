3a2249e5024c83d2a626e2c8b34bb85b2f2d0b18
16348104 <16348104@qq.com>
2017-02-05 12:51:10 +0800

    update20

--- 20/demo1.js
+++ 20/demo1.js
@@ -2,6 +2,23 @@ window.onload = function () {
     // alert(Node.ELEMENT_NODE);
     // alert(Node.TEXT_NODE);
     // alert(document);
-    alert(document.documentElement);
-    alert(document.nodeType);
+    // alert(document.documentElement);
+    // alert(document.nodeType);
+    // alert(document.title);
+    // alert(document.URL);
+/*    var box = document.createElement('div');
+    var t1 = document.createTextNode('Mr.');
+    var t2 = document.createTextNode('Lee!');
+    box.appendChild(t1);
+    box.appendChild(t2);
+    document.body.appendChild(box);
+    box.normalize();
+   */
+    // alert(box.childNodes.length);
+    document.getElementById('li').scrollIntoView();
+    // var box= document.getElementById('d');
+    // alert(box.children.length);
+    var box= document.getElementById('box');
+    alert(box.contains(box.firstChild));
+
 };
