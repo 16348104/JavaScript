0942c5c41a244d1891f8005f727b2475809b1cc3
16348104 <16348104@qq.com>
2017-02-06 10:28:56 +0800

    update 21

--- 22/demo1.js
+++ 22/demo1.js
@@ -6,6 +6,10 @@ window.onload = function () {
     var box = document.getElementById('box');
     var sheet = document.styleSheets[0];
     var rule = (sheet.cssRules || sheet.rules)[0];
-    alert(rule.style.width);
-    alert(rule.style.height);
+    /*alert(rule.style.width);
+    alert(rule.style.height);*/
+    alert(box.clientHeight);
+    alert(box.clientWidth);
+    //返回number类型,不包括外边距,边框,滚动条大小
+    //内边距会增加到返回值中
 };
