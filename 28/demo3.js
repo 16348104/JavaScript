29f9e8de23630c531e81d32de5bcc74155e56fb6
16348104 <16348104@qq.com>
2017-02-10 17:26:54 +0800

    update 28

--- 28/demo3.js
+++ 28/demo3.js
@@ -7,3 +7,10 @@ console.error('错误');
 console.log('日志');
 console.info('信息');
 console.warn('警告');*/
+
+var n1=1;
+console.log('n1=' + n1);
+var n2 = 'b';
+console.log('n2=' + n2);
+var r = n1 + n2;
+alert(r);
