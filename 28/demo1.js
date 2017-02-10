23858b242b67c0913980d2aa203d851057270f00
16348104 <16348104@qq.com>
2017-02-10 15:41:08 +0800

    update 28

--- 28/demo1.js
+++ 28/demo1.js
@@ -1,12 +1,13 @@
-
 /*
+
+/!*
 try {
    window.abc();
 } catch (e) {
     alert(e.name);
     alert(e.message);
 }
-*/
+*!/
 function box() {
     try {
         var a = {};
@@ -18,4 +19,13 @@ function box() {
         a = null;
     }
 }
-box();
+box();*/
+try {
+    new 10;
+} catch (e) {
+    if (e instanceof TypeError) {
+        alert('错误类型：' + e.message);
+    } else {
+        alert('未知错误'+e);
+    }
+}
