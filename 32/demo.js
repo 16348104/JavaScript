801c54df24894c689034b2b6a74b83292763aa45
16348104 <16348104@qq.com>
2017-02-12 18:44:56 +0800

    update32

---
+++ 32/demo.js
@@ -0,0 +1,28 @@
+/**
+ * Created by dell on 2017/2/12.
+ */
+
+//json数组
+'{"name":"lee", "age":100}'
+
+//json对象
+'["100",true,"lee"]'
+
+
+
+
+//json常用结构
+    [
+    {
+        "title" : "a",
+        "num" : 1
+    },
+    {
+        "title" : "b",
+        "num" : 1
+    },
+    {
+        "title" : "c",
+        "num" : 1
+    }
+    ]
