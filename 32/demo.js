b60394d33bb33c587c7b9765e72c00f56d1696de
16348104 <16348104@qq.com>
2017-02-12 19:07:27 +0800

    update32

--- 32/demo.js
+++ 32/demo.js
@@ -1,28 +1,47 @@
-/**
+/*
+ /!**
  * Created by dell on 2017/2/12.
- */
+ *!/
 
-//json数组
-'{"name":"lee", "age":100}'
+ //json数组
+ '{"name":"lee", "age":100}'
 
-//json对象
-'["100",true,"lee"]'
-
-
+ //json对象
+ '["100",true,"lee"]'
 
 
-//json常用结构
-    [
+
+
+ //json常用结构
+ [
+ {
+ "title" : "a",
+ "num" : 1
+ },
+ {
+ "title" : "b",
+ "num" : 1
+ },
+ {
+ "title" : "c",
+ "num" : 1
+ }
+ ]*/
+/*
+ var json = '[{"title":"a","num":"1"},{"title":"b","num":"1"}]';
+ //alert(typeof JSON);
+ var box = JSON.parse(json);
+ alert(box[0].title);*/
+
+var box = [
     {
-        "title" : "a",
-        "num" : 1
+        title: 'a',
+        num: 1
     },
     {
-        "title" : "b",
-        "num" : 1
-    },
-    {
-        "title" : "c",
-        "num" : 1
+        title: 'b',
+        num: 1
     }
-    ]
+];
+var json = JSON.stringify(box);
+alert(json);
