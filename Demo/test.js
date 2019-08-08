b79414b67681c7b9e2a98ac793f7fab5f4b29bf0
16348104 <16348104@qq.com>
2019-08-08 10:20:11 +0800

    update  demo

--- Demo/test.js
+++ Demo/test.js
@@ -6,21 +6,21 @@ document.getElementById('nrStr').value = new Date().toLocaleString();
 // setTimeout(function () {
 //     alert("Hello");
 // }, 3000);
-function f(Max) {
+var aud = document.getElementsByTagName('audio').length;
+console.log('audio个数：', aud);
+var vid = document.getElementsByTagName('video').length;
+console.log('video个数：', vid);
+function f_Ran(Max) {
     return Math.floor(Math.random() * Max)
 }
-var audio = document.getElementsByTagName('audio').length;
-console.log('audio个数：', audio);
-var video = document.getElementsByTagName('video').length;
-console.log('video个数：', video);
-console.log("随机", f(video));
-console.log("随机", f(audio));
-var v = document.getElementsByTagName('video')[f(video)];
-var a = document.getElementsByTagName('audio')[f(audio)];
+console.log("视频随机", f_Ran(vid));
+console.log("音频随机", f_Ran(aud));
+var v = document.getElementsByTagName('video')[f_Ran(vid)];
 v.play();
+var a = document.getElementsByTagName('audio')[f_Ran(aud)];
 a.play();
 
-// alert(video);
+
 
 // video.play();
 var b = document.getElementsByClassName('vjs-big-play-button')[0];
